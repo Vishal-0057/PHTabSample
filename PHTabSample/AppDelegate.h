6a8a8e058b098670f93eb870a46beae245651528
VISHAL GUPTA <vishal.gupta@intelligrape.com>
2015-05-19 22:43:42 +0530

    Initial Commit

---
+++ PHTabSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHTabSample
+//
+//  Created by abhayam rastogi on 5/19/15.
+//  Copyright (c) 2015 Intelligrape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PHTabSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHTabSample
+//
+//  Created by abhayam rastogi on 5/19/15.
+//  Copyright (c) 2015 Intelligrape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
